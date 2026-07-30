7bef7552fe8cc3b0f4d0b9fbbd885956e8506615
Miguel <miguelcokerdelgado@rhs.school.nz>
2026-07-31 10:53:00 +1200

    Added exit button

--- menu.lua
+++ menu.lua
@@ -5,7 +5,8 @@ local button = require("button")
 function menu.load()
     menu.buttons = {
         start = button.new("Play", 10, 10, 40, 20, function() GameStates.play = true end),
-        pause = button.new("Pause", 60, 10, 40, 20, function() GameStates.pause = not GameStates.pause end)
+        pause = button.new("Pause", 60, 10, 40, 20, function() GameStates.pause = not GameStates.pause end),
+        exit = button.new("Exit", 110, 10, 40, 20, love.event.quitdddd)
     }
 end
 
