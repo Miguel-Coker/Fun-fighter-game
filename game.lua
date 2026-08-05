723c3f2fe0376b8abc2ee78a8abc50c1111ed54f
Miguel <miguelcokerdelgado@rhs.school.nz>
2026-08-06 10:51:03 +1200

    Added camera zoomng

--- game.lua
+++ game.lua
@@ -10,11 +10,16 @@ game.cameraModes = {
     FIXED = 0,
     FOLLOW = 1
 }
-
+local cameraOffsetX
+local cameraOffsetY
 function game.load()
     game.camera.x = 0
     game.camera.y = 0
     game.camera.speed = 10
+    game.camera.zoom = 1.2
+
+    cameraOffsetX = (love.graphics.getWidth() / 2 - 20) / game.camera.zoom
+    cameraOffsetY = (love.graphics.getHeight() / 1.5) / game.camera.zoom
 end
 
 ---@param mode game.cameraModes
@@ -39,16 +44,14 @@ function game.camera:screenY(y)
     return y + self.y
 end
 
-local cameraOffsetX = love.graphics.getWidth() / 2
-local cameraOffsetY = love.graphics.getHeight() / 1.5
-
 function game.update(dt)
     game.camera:attach(player.player.hurtBox.body:getX() - cameraOffsetX, player.player.hurtBox.body:getY() - cameraOffsetY, game.cameraModes.FOLLOW, dt)
 end
 
 function game.draw()
     love.graphics.push()
-    love.graphics.translate(-game.camera.x, -game.camera.y)
+    love.graphics.translate(-game.camera.x * game.camera.zoom, -game.camera.y * game.camera.zoom)
+    love.graphics.scale(game.camera.zoom, game.camera.zoom)
     love.graphics.setColor(0, 1, 0)
     love.graphics.polygon("fill", Floor.body:getWorldPoints(Floor.shape:getPoints()))
     love.graphics.polygon("fill", LeftWall.body:getWorldPoints(LeftWall.shape:getPoints()))
